32b6f867cb524a03e6b91d1093cae3cbd07d6118
ggjorven <jorbenvanderwal@gmail.com>
2026-05-21 14:47:45 +0200

    Add AppError

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
+use core::fmt;
+
 #[macro_use]
 mod logging;
 mod env;
 
-fn main() -> Result<(), ()> {
+#[derive(Debug)]
+enum AppError {
+    EnvError(env::EnvError),
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            AppError::EnvError(error) => {
+                write!(f, "{}", error)
+            },
+        }
+    }
+}
+
+fn main() -> Result<(), AppError> {
     logging::add_sink(Box::new(logging::ConsoleSink::new(None)));
 
-    let result = env::EnvOptions::from_env();
-    let Ok(env) = result else {
-        error!("{:?}", result.unwrap_err());
-        return Err(());
-    };
+    let env = env::EnvOptions::from_env().map_err(|error| {
+        return AppError::EnvError(error);
+    })?;
 
-    info!("Env options: {:?}", env);
+    trace!("Env options: {:?}", env);
 
     loop {}
 
