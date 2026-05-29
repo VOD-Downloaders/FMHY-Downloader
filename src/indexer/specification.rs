0c974b39be607576dda0a004eeb65ff400eaa818
ggjorven <jorbenvanderwal@gmail.com>
2026-05-29 17:59:11 +0200

    Make IndexerType cleaner using seperate structs.

--- src/indexer/specification.rs
+++ src/indexer/specification.rs
@@ -1,29 +1,31 @@
 use serde::{Serialize, Deserialize};
 
 /////////////////////////////////////////////////////
+// Specifications
+/////////////////////////////////////////////////////
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IndexSpecification {
+    pub wait_time: u32,
+    pub retries: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MP4Specification {
+    pub wait_time: u32,
+    pub retries: u32,
+}
+
+/////////////////////////////////////////////////////
 // IndexerType
 /////////////////////////////////////////////////////
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum IndexerType {
     #[serde(rename = "index")]
-    IndexInterception {
-        #[serde(default = "default_wait_time")]
-        wait_time: u8,
-        #[serde(default = "default_retries")]
-        retries: u8,
-    },
+    IndexInterception(IndexSpecification),
 
     #[serde(rename = "mp4")]
-    MP4Interception,
-}
-
-fn default_wait_time() -> u8 {
-    6
-}
-
-fn default_retries() -> u8 {
-    5
+    MP4Interception(MP4Specification),
 }
 
 /////////////////////////////////////////////////////
